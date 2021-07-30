df27bf03bea5a79bed7a993dd4b3ee0f18b8f327
BooMajka <visionseers@gmail.com>
2021-07-30 16:27:03 -0500

    start

---
+++ firebase.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDJ5aeaz2sR1sI1kLhhq5x_jc_ZRX_ZnkA",
+    authDomain: "eclone-f13d5.firebaseapp.com",
+    projectId: "eclone-f13d5",
+    storageBucket: "eclone-f13d5.appspot.com",
+    messagingSenderId: "767993775089",
+    appId: "1:767993775089:web:dbd9d2069c5870fe36fa50"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
